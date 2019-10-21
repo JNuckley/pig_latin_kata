5e233de6da6402aa3e61283b1a88965e25e73b8b
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-10-21 17:17:01 +0100

    Question 3

--- exercise_3.rb
+++ exercise_3.rb
@@ -6,6 +6,29 @@
 
 def pig_latin(word)
 
+if word.start_with?("a", "e", "i","o", "u")
+return word + "way"
+elsif word.start_with?("qu")
+word = word[2..-1]
+return word + "qu" + "ay"
+
+else
+  first_letters = []
+  letter = word.split("")
+
+  letter.each do |x|
+
+    if x.start_with?("a", "e", "i","o", "u")
+    return word  + first_letters.join + "ay"
+    break
+    else
+      first_letters << x
+      word = word[1..-1]
+
+
+end
+end
+end
 end
 
 ## Tests:
