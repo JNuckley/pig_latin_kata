8d2ea8b632abdd09dd8c6d091122b15398a038ac
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-10-21 17:19:34 +0100

    Question 4

--- exercise_4.rb
+++ exercise_4.rb
@@ -6,7 +6,44 @@
 # other formats
 
 def pig_latin(word)
+if word.capitalize == word
+  cap = true
+else
+  cap = false
+end
+if word.start_with?("a", "e", "i","o", "u")
+return word + "way"
+elsif word.start_with?("qu")
+word = word[2..-1]
+if cap == true
+  return word.capitalize + "qu" + "ay"
+else
+return word + "qu" + "ay"
+end
 
+else
+  first_letters = []
+  letter = word.split("")
+
+  letter.each do |x|
+
+    if x.start_with?("a", "e", "i","o", "u")
+      if cap == true
+    return word.capitalize  + first_letters.join + "ay"
+    break
+    else
+      return word  + first_letters.join + "ay"
+      break
+    end
+
+    else
+      first_letters << x.downcase
+      word = word[1..-1]
+
+
+end
+end
+end
 end
 
 ## Tests:
